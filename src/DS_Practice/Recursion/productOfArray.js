93ee5d539003058b19a826eeee196270b5ce8c7c
Chandresh Patidar <chandreshpatidar5@gmail.com>
2022-10-11 19:13:56 +0530

    Calculate product of array elements using recursion

---
+++ src/DS_Practice/Recursion/productOfArray.js
@@ -0,0 +1,14 @@
+/**
+ * Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+ */
+
+function productOfArray(arr) {
+  if (arr.length === 1) return arr[0];
+  return arr[0] * productOfArray(arr.splice(1));
+}
+
+console.log('Should print 6 ->', productOfArray([1, 2, 3]));
+console.log('Should print 60 ->', productOfArray([1, 2, 3, 10]));
+
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
